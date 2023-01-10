316f640469250085cc1f72d15f80814afab2bf3c
Pavel Ostapchuk <paviv84@gmail.com>
2023-01-10 12:58:38 +0300

    10.01.2023

---
+++ Les2AlgotitmEffency/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+// 1 5 4 1 3 4 7 4 1 2
+// m=3
+// max -> 15
+// найти макс сумму трех подряд идущих элементов в массиве
+Console.Clear();
+int size = 10;
+int m = 2;
+int[] array = Enumerable.Range(1, size)
+                        .Select(item => Random.Shared.Next(10))
+                        .ToArray();
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Stopwatch sw = new();
+sw.Start();
+int max = 0;
+for (int i = 0; i < m; i++)
+{
+    max += array[i];
+}
+int max1 = max;
+for (int j = 0; j < array.Length - m; j++)
+{
+    max1 = max1 - array[j] + array[j + m];
+    if (max1 > max) max = max1;
+    Console.WriteLine(max1);
+}
+sw.Stop();
+Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
+Console.WriteLine(max);
---
+++ Les3BigONotation/Program.cs
@@ -0,0 +1,51 @@
+﻿// int n = 5;
+// int [] array = new int[n];
+// for (int i = 0; i < n; i++)
+// {
+//     array[i]= Convert.ToInt32(Console.ReadLine());
+// }
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+// Console.WriteLine(array[3]);
+// // время работы программы 
+// // сложность алгоритма - О Нотация (O Notation) 
+// // данный алгоритм работает O(1) - количестов действий, что бы узнать конечный резльтат - 1 действие.
+
+// int sum = 0;
+// for (int i = 0; i < n; i++)
+// {
+//     sum+=array[i];
+// }
+// Console.WriteLine(sum);
+// // в данном алгоритем: 0(n) или О(5) - при n = 5.
+
+int m = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        Console.Write($"{(j + 1) * (i + 1)}\t");// \t - Для вывода ронвыми колонками церез табуляцию
+    }
+    Console.WriteLine();
+}
+// O(n^2)
+
+int[,] matrix = new int[m, m];
+for (int i = 0; i < m; i++)
+{
+    for (int j = i; j < m; j++)
+    {
+        matrix[i, j] = (i + 1) * (j + 1);
+        //matrix[j, i] = (i + 1) * (j + 1);
+        matrix[j, i] = matrix[i, j];
+    }
+}
+Console.WriteLine();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        Console.Write($"{matrix[i, j]}\t");// \t - Для вывода ронвыми колонками церез табуляцию
+    }
+    Console.WriteLine();
+}
+// O(n^2/2)
