18b8541747ae0bda5c2de64c5e88a00706a77458
Pavel Ostapchuk <paviv84@gmail.com>
2023-01-12 13:15:40 +0300

    Организация струтктуры проекта

--- Les4AlgoSortSelect/Program.cs
+++ Les4AlgoSortSelect/Program.cs
@@ -1,21 +1,8 @@
 ﻿// сортировка выбором
-
-int[]array = {4, 5, 3, 6, 1, 7, 2, 3};
-Console.WriteLine($"[{String.Join(' ',array)}]");
+using static Sortings;
+using static Infrastructure;
+int[]array = CreateArray(10);
+PrintArray (array);
 SelectionSort(array);
-Console.WriteLine($"[{String.Join(' ',array)}]");
-void SelectionSort(int[] collection)
-{
-    int size = collection.Length;
-    for (int i = 0; i < size - 1; i++)
-    {
-        int pos = i;
-        for (int j = i + 1; j < size; j++)
-        {
-            if (collection[j] < collection[pos]) pos = j;
-        }
-        int temp = collection[i];
-        collection[i]=collection[pos];
-        collection[pos]=temp;
-    }
-}
+PrintArray (array);
+
