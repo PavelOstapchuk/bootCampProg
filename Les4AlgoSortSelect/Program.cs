129aeccbea0dd04a7c4f0cc780133b3d572ae849
Pavel Ostapchuk <paviv84@gmail.com>
2023-01-12 12:53:07 +0300

    добвлен простой пример сортировки выбором

--- Les4AlgoSortSelect/Program.cs
+++ Les4AlgoSortSelect/Program.cs
@@ -1,1 +1,21 @@
 ﻿// сортировка выбором
+
+int[]array = {4, 5, 3, 6, 1, 7, 2, 3};
+Console.WriteLine($"[{String.Join(' ',array)}]");
+SelectionSort(array);
+Console.WriteLine($"[{String.Join(' ',array)}]");
+void SelectionSort(int[] collection)
+{
+    int size = collection.Length;
+    for (int i = 0; i < size - 1; i++)
+    {
+        int pos = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (collection[j] < collection[pos]) pos = j;
+        }
+        int temp = collection[i];
+        collection[i]=collection[pos];
+        collection[pos]=temp;
+    }
+}
